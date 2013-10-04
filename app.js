66a4ce3b2e87fb0b8bd64fe5eaa52f9d8708d398
Alan Rodriguez <alanrodriguezromero@gmail.com>
2013-10-04 18:11:53 -0500

    Added depenencies

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 var express = require('express');
-var app = express.createServer();
+var jsdom = require("jsdom").jsdom;
+var app = express();
+
+var config = {
+	'host' : 'https://news.ycombinator.com/',
+}
+
+var helper = {
+	getNews : function(){
+		var walk = function(node){
+			if(node.nodeType === 1 && node.tagName.toLowerCase() === "td" 
+				&& node.classList.contains("title")){
+				// Procesa dependiendo si es 
+				console.log(node.innerHTML);
+			}
+			var child = node.firstChild;
+			while(child){
+				walk(child);
+				child = child.nextSibling;
+			}
+		}
+	}
+}
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+	res.type('application/json');
+	res.send(JSON.stringify({
+		name: "HackerNewsAPI",
+		description: "Hacker News API",
+		version: "0.0.1"
+	}, null, '\t'));
 });
 
-app.listen();
-console.log('Express server started on port %s', app.address().port);
+app.get('/news', function(req, res){
+
+	jsdom.env(config.host, function(err, window){
+		var html = window.document.querySelector("html");
+		console.log(html);
+	});
+
+		
+});
+
+app.listen(3000);
